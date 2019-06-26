76f36f07e3f0dc9f5e78ce1c573758bbc36d2804
Saju Paul <saju.christel@blu-pa.com>
2019-06-26 21:18:47 +0530

    dynamic filtering

--- src/main/java/com/in2minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/in2minutes/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,5 +1,8 @@
 package com.in2minutes.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +22,27 @@ public class FilteringController {
         return Arrays.asList(new SomeBean("value1", "value2", "value3"),
                 new SomeBean("value12", "value22", "value32"));
     }
+
+    @GetMapping("/filtering-dynamic")
+    public MappingJacksonValue retrieveSomeBeanDynamic() {
+        DynamicBean dynamicBean = new DynamicBean("value1", "value2", "value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(dynamicBean);
+        mappingJacksonValue.setFilters(new SimpleFilterProvider()
+                .addFilter("dynamicBeanFilter",
+                        SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2")));
+        return mappingJacksonValue;
+    }
+
+
+    @GetMapping("/filtering-list-dynamic")
+    public MappingJacksonValue retrieveSomeBeansDynamic() {
+        List<DynamicBean> dynamicBeans = Arrays.asList(new DynamicBean("value1", "value2", "value3"),
+                new DynamicBean("value12", "value22", "value32"));
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(dynamicBeans);
+        mappingJacksonValue.setFilters(new SimpleFilterProvider()
+                .addFilter("dynamicBeanFilter",
+                        SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3")));
+        return mappingJacksonValue;
+    }
+
 }
