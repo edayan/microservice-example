18225f4884414d929f8d0398f7f4f9d8ded2fc3e
Saju Paul <saju.christel@blu-pa.com>
2019-06-27 20:53:57 +0530

    One to many mapping

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ insert into user values (1001, sysdate(), 'AB');
 insert into user values (1002, sysdate(), 'JACK');
 insert into user values (1003, sysdate(), 'JILL');
 insert into user values (1004, sysdate(), 'SAJU');
+
+insert into post values (11001, 'My First Post', 1001);
+insert into post values (11002, 'My Second Post', 1001);
+insert into post values (11003, 'Jacks First Post', 1002);
